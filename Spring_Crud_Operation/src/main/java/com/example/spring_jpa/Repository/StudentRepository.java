734ac245bad669e17372d2b949c71ec81ff33167
Md.Shahriyar Chowdhury Shovon <36441192+shovon26@users.noreply.github.com>
2022-02-28 18:32:45 +0600

    Delete Spring_Crud_Operation directory

--- Spring_Crud_Operation/src/main/java/com/example/spring_jpa/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring_jpa.Repository;
-
-import com.example.spring_jpa.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository <Student, Integer>{
-
-}
