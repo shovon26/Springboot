c65ed67e1114775e17185c64cfb49e6133e8d923
Md.Shahriyar Chowdhury Shovon <36441192+shovon26@users.noreply.github.com>
2022-03-01 11:32:27 +0600

    Delete OneToOne directory

--- OneToOne/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.relational.Repository;
-
-import com.example.relational.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
